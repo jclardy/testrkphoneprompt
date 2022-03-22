188edb2996d64a303401db7994c2d5a7b353e859
jclardy <jason@swiftfoxsoftware.com>
2022-03-22 15:11:38 -0400

    First test

---
+++ phone.js
@@ -0,0 +1,10 @@
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const programmingCode = urlParams.get('code');
+
+window.onload = function() {
+    const codeLink = "<a href=\"telprompt:" + programmingCode + "\">Dial Programming Code</a>";
+    document.getElementById('output').innerHTML = codeLink;
+};
